27038ad05e72e554405b4c78ec6dd0185380f11b
Hrushikesh Makode <makodehrushikesh@gmail.com>
2021-03-07 14:12:55 +0530

    Chat App is the name of app

--- App.js
+++ App.js
@@ -2,11 +2,7 @@ import React from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import {
-	StatusBar,
-	StyleSheet,
-	Platform,
-} from "react-native";
+import { StatusBar, StyleSheet, Platform } from "react-native";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import HomeScreen from "./screens/HomeScreen";
@@ -23,9 +19,7 @@ const globalScreenOptions = {
 export default function App() {
 	return (
 		<NavigationContainer style={styles.container}>
-			<Stack.Navigator
-				screenOptions={globalScreenOptions}
-			>
+			<Stack.Navigator screenOptions={globalScreenOptions}>
 				<Stack.Screen name="Login" component={LoginScreen}></Stack.Screen>
 				<Stack.Screen name="Register" component={RegisterScreen}></Stack.Screen>
 				<Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
